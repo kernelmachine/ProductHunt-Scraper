7df779263ff7fe2da77617c23ac79c1cf414505a
ssgrn <sgrangan01@gmail.com>
2014-12-25 17:06:24 -0500

    added producthunt scraper code

---
+++ producthunt.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+
+class ProductHunt():
+
+	def parse_html(self,url):
+		html_doc = requests.get(url)	
+		return BeautifulSoup(html_doc.text)
+
+	def products(self,today):
+		print("parsing html...")
+	 	b = self.parse_html("http://www.producthunt.com/")
+	 	print("done.")
+		t = [x.find_all('a',class_="post-url title") for x in b.find_all('div',class_="day")][0]
+	 	h = [x.find_all('a',class_="post-url title",href=True) for x in b.find_all('div',class_="day")][0]
+	 	s = [x.find_all('span',class_="post-tagline description") for x in b.find_all('div',class_="day")][0]
+	 	v = [x.find_all('span',class_="vote-count") for x in b.find_all('div',class_="day")][0]
+	 	products = [("product",x.get_text()) for x in t]
+	 	descriptions = [("description",x.get_text()) for x in s]
+	 	upvotes = [("upvote",int(x.get_text())) for x in v]
+	 	links = [("link","http://www.producthunt.com"+x['href']) for x in h]
+	 	result = [dict([w] + [x] + [y] + [z]) for (w,x,y,z) in zip(products, descriptions,upvotes,links)]
+		return result
